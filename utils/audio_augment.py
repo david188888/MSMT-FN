05ffa96ca65ee81d52469f2f62fdab7d9c5033c6
David <1697252093@qq.com>
2024-08-11 22:16:06 +0800

    add the audio augment

--- utils/audio_augment.py
+++ utils/audio_augment.py
@@ -10,19 +10,18 @@ import re
 class GaussianAugmentation:
     def __init__(self):
         self.augment = Compose([
-            AddGaussianNoise(min_amplitude=0.001, max_amplitude=0.015, p=0.5),
-            AddGaussianSNR(min_snr_db=5.0, max_snr_db=40.0, p=0.5)
+            AddGaussianNoise(min_amplitude=0.003, max_amplitude=0.01, p=1.0),
+            AddGaussianSNR(min_snr_db=10.0, max_snr_db=40.0, p=1.0)
         ])
 
     def __call__(self, audio, sample_rate):
         return self.augment(samples=audio, sample_rate=sample_rate)
 
-
 class TemporalPitchAugmentation:
     def __init__(self):
         self.augment = Compose([
-            TimeMask(min_band_part=0.1, max_band_part=0.1, p=0.5),
-            PitchShift(min_semitones=-2, max_semitones=2, p=0.5)
+            TimeMask(min_band_part=0.1, max_band_part=0.1, p=1.0),
+            PitchShift(min_semitones=-1.2, max_semitones=1.2, p=1.0)
         ])
 
     def __call__(self, audio, sample_rate):
@@ -32,8 +31,8 @@ class TemporalPitchAugmentation:
 class ShiftNormalizeAugmentation:
     def __init__(self, min_shift=-0.2, max_shift=0.2):
         self.augment = Compose([
-            Shift(min_shift=min_shift, max_shift=max_shift, p=0.5),
-            Normalize(p=0.5)
+            Shift(min_shift=min_shift, max_shift=max_shift, p=1.0),
+            Normalize(p=0.1)
         ])
 
     def __call__(self, audio, sample_rate):
@@ -41,24 +40,26 @@ class ShiftNormalizeAugmentation:
 
 
 class AudioAugmentation:
-    def __init__(self, min_shift=-0.1, max_shift=0.1):
+    def __init__(self, min_shift=-0.2, max_shift=0.2):
         self.shift_normalize_augmenter = ShiftNormalizeAugmentation(
             min_shift, max_shift)
 
-    def __call__(self, audio, sample_rate=16000):
+    def __call__(self, audio, sample_rate):
         audio = self.shift_normalize_augmenter(audio, sample_rate)
         return audio
 
 
 if __name__ == '__main__':
-    csv_path = "/home/lhy/MM-LLMs/MM-purchase-judgment/MMML/data/Customer/trainlabel.csv"
-    audio_directory = "/home/lhy/MM-LLMs/MM-purchase-judgment/MMML/data/Customer/train"
+    csv_path = "/home/lhy-scnu/mmml/train/dialog_test_11.csv"
+    audio_directory = "/home/lhy-scnu/mmml/train/dialog"
     df = pd.read_csv(csv_path)
     
     encoder = LabelEncoder()
     targets = encoder.fit_transform(list(df['label']))
     texts = list(df['text'])
-    audio_file_paths = [f"{audio_directory}/{audio_id}.wav" for audio_id in df['audio_id']]
+    phones = list(df['phone'])
+    clips_id = list(df['Segment_number'])
+    audio_file_paths = [f"{audio_directory}/{audio_id}" for audio_id in df['audio_id']]
     
     
     gaussian_augment = GaussianAugmentation()
@@ -69,62 +70,89 @@ if __name__ == '__main__':
     augmented_texts = []
     augmented_targets = []
     augmented_audio_file_paths = []
+    augmented_phones = []
+    augmented_clips = []
 
     # Perform exponential augmentation
     for i in range(len(audio_file_paths)):
         current_audio, _ = torchaudio.load(audio_file_paths[i])
         current_audio = current_audio.detach().numpy()
-        print(current_audio)
+        
         label = targets[i]
-        num_augmentations = 2
-        phone = audio_file_paths[i]
-        # 除去尾缀wav
-        match = re.match(r'(.*)\.wav', phone)
+        phone = phones[i]
+
+        clip_id = clips_id[i]
+        audio_name = audio_file_paths[i]
+        filename = re.search(r'[^/]+$', audio_name).group()
+        match = re.match(r'(.*)\.wav', audio_name)
         if match:
-            phone = match.group(1)
+            audio_name = match.group(1)
         text = texts[i]
+
+
 
         # Store the original data
         augmented_texts.append(text)
         augmented_targets.append(label)
-        augmented_audio_file_paths.append(audio_file_paths[i])
+        augmented_audio_file_paths.append(filename)
+        augmented_phones.append(phone)
+        augmented_clips.append(clip_id)
 
         # Perform augmentations
-        for _ in range(num_augmentations):
-            labelx = np.random.rand(1)
-            print(labelx)
-            if labelx < 0.3:
-                current_audio = gaussian_augment(
+        if label != 3:
+                current_audio_1 = gaussian_augment(
                     current_audio, sample_rate=16000)
-                augment_type = "gaussian"
-            elif labelx > 0.3 and labelx < 0.7:
-                current_audio = temporal_pitch_augment(
-                    current_audio, sample_rate=16000)
-                augment_type = "temporal"
-            else:
-                current_audio = audio_augment(current_audio)
-                augment_type = "shiftnormalize"
-            
+                augment_type_1 = "gaussian"
 
-            # Convert to tensor and save to a temporary file
-            temp_path = f"{phone}_{augment_type}.wav"
-            dir = "/home/lhy/MM-LLMs/MM-purchase-judgment/MMML/data/Customer/train_augmented"
-            torchaudio.save(f"{dir}/{temp_path}", torch.tensor(current_audio), 16000)
-            # torchaudio.save(temp_path, torch.tensor(current_audio), 16000)
-            # Store the augmented data
+                current_audio_2 = temporal_pitch_augment(
+                    current_audio, sample_rate=16000)
+                augment_type_2 = "temporal"
+                
+                current_audio_3 = audio_augment(current_audio)
+                augment_type_3 = "shiftnormalize"
+                
+                temp_path_1 = f"{audio_name}_{augment_type_1}.wav"
+                temp_path_2 = f"{audio_name}_{augment_type_2}.wav"
+                temp_path_3 = f"{audio_name}_{augment_type_3}.wav"
+                
+                # dir = "/home/lhy-scnu/mmml/verify/verify_augmented"
+                torchaudio.save(f"{temp_path_1}", torch.tensor(current_audio_1), 16000)
+                torchaudio.save(f"{temp_path_2}", torch.tensor(current_audio_2), 16000)
+                torchaudio.save(f"{temp_path_3}", torch.tensor(current_audio_3), 16000)
+
+                # Store the augmented data
+                for i in range(1,4):
+                    augmented_texts.append(text)
+                    augmented_targets.append(label)
+                    augmented_phones.append(phone)
+                    augmented_clips.append(clip_id)
+                    temp_path = eval(f"temp_path_{i}")
+                    augmented_audio_file_paths.append(temp_path)
+                    
+        else:
+            current_audio = gaussian_augment(current_audio,16000)
+            temp_path = f"{audio_name}_gaussian.wav"
+            augment_type = "gaussian"
+            torchaudio.save(temp_path, torch.tensor(current_audio), 16000)
+            
             augmented_texts.append(text)
+            augmented_phones.append(phone)
+            augmented_clips.append(clip_id)
             augmented_targets.append(label)
             augmented_audio_file_paths.append(temp_path)
+        
+            # Convert to tensor and save to a temporary file
 
 
-            
+    print('Augmentation finished')
+
+
     # 将增强后的数据保存到csv文件
     augmented_df = pd.DataFrame({
-        'audio_id': [i for i in augmented_audio_file_paths],
+        'phone': augmented_phones,
+        'audio_id': augmented_audio_file_paths,
+        'segment_number': augmented_clips,
         'text': augmented_texts,
         'label': augmented_targets
     })
-
-    augmented_df.to_csv("/home/lhy/MM-LLMs/MM-purchase-judgment/MMML/data/Customer/train_augmented.csv", index=False)
-    
-    # 将增强后的数据
+    augmented_df.to_csv("/home/lhy-scnu/mmml/train/train_augment.csv", index=False)
--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,5 +1,7 @@
 import torch
 from torch import nn
+import torch.utils
+import torch.utils.data
 import transformers
 import torchaudio
 # import audiomentations
@@ -47,7 +49,7 @@ import os
 #         return len(self.phone_to_dialogue)
 
 
-class Dataset_audio_text(torch.utils.data.Dataset):
+class QA_Dataset(torch.utils.data.Dataset):
     def __init__(self, csv_path, audio_directory):
         df = pd.read_csv(csv_path)
 
@@ -100,7 +102,7 @@ class Dataset_audio_text(torch.utils.data.Dataset):
 
         # extract audio features
             features = self.feature_extractor(
-                soundData, sampling_rate=8000, max_length=320000,
+                soundData, sampling_rate=8000, max_length=160000,
                 return_attention_mask=True, truncation=True, padding="max_length"
             )
             audio_features.append(torch.tensor(np.array(features['input_values']), dtype=torch.float32).squeeze())
@@ -123,62 +125,310 @@ class Dataset_audio_text(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.phone_groups)
-
-
-def collate_fn(batch):
-    text_tokens = []
-    text_masks = []
-    audio_inputs = []
-    audio_masks = []
-
-    targets_M = []
-
-    # organize batch
-    for i in range(len(batch)):
-        # text
-        text_tokens.append(batch[i]['text_tokens'])
-        text_masks.append(batch[i]['text_masks'])
-        # audio
-        audio_inputs.append(batch[i]['audio_inputs'])
-        audio_masks.append(batch[i]['audio_masks'])
-
-       # labels
-        targets_M.append(batch[i]['target']['M'])
-        
-    # print(f"size of text_tokens: {len(text_tokens), len(text_tokens[0]), len(text_tokens[0][0])}")
-    # print(f"size of audio_inputs: {len(audio_inputs), len(audio_inputs[0]), len(audio_inputs[0][0])}")
-    # print(f"targets_M: {targets_M}")
     
-    text_tokens = torch.nn.utils.rnn.pad_sequence([tt.clone().detach().to(dtype=torch.long) for tt in text_tokens], batch_first=True)
-    text_masks = torch.nn.utils.rnn.pad_sequence([tm.clone().detach().to(dtype=torch.long) for tm in text_masks],batch_first=True)
-    audio_inputs = torch.nn.utils.rnn.pad_sequence([ai.clone().detach() for ai in audio_inputs], batch_first=True)
-    audio_masks = torch.nn.utils.rnn.pad_sequence([am.clone().detach() for am in audio_masks],batch_first=True)
-    
-    
-    text_inputs = text_tokens.view(text_tokens.size(0)*text_tokens.size(1), text_tokens.size(2))
-    text_masks = text_masks.view(text_masks.size(0)*text_masks.size(1), text_masks.size(2))
-    audio_inputs = audio_inputs.view(audio_inputs.size(0)*audio_inputs.size(1), audio_inputs.size(2))
-    audio_masks = audio_masks.view(audio_masks.size(0)*audio_masks.size(1), audio_masks.size(2))
+    def collate_fn(batch):
+        text_tokens = []
+        text_masks = []
+        audio_inputs = []
+        audio_masks = []
 
-    
+        targets_M = []
 
-    return {
-        # text
-        "text_tokens": text_inputs,
-        "text_masks": text_masks,
-        # audio
-        "audio_inputs": audio_inputs,
-        "audio_masks": audio_masks,
+        # organize batch
+        for i in range(len(batch)):
+            # text
+            text_tokens.append(batch[i]['text_tokens'])
+            text_masks.append(batch[i]['text_masks'])
+            # audio
+            audio_inputs.append(batch[i]['audio_inputs'])
+            audio_masks.append(batch[i]['audio_masks'])
+
         # labels
-        "targets": 
-            torch.tensor(targets_M, dtype=torch.float32),
-    }
+            targets_M.append(batch[i]['target']['M'])
+            
+        # print(f"size of text_tokens: {len(text_tokens), len(text_tokens[0]), len(text_tokens[0][0])}")
+        # print(f"size of audio_inputs: {len(audio_inputs), len(audio_inputs[0]), len(audio_inputs[0][0])}")
+        # print(f"targets_M: {targets_M}")
+        
+        text_tokens = torch.nn.utils.rnn.pad_sequence([tt.clone().detach().to(dtype=torch.long) for tt in text_tokens], batch_first=True)
+        text_masks = torch.nn.utils.rnn.pad_sequence([tm.clone().detach().to(dtype=torch.long) for tm in text_masks],batch_first=True)
+        audio_inputs = torch.nn.utils.rnn.pad_sequence([ai.clone().detach() for ai in audio_inputs], batch_first=True)
+        audio_masks = torch.nn.utils.rnn.pad_sequence([am.clone().detach() for am in audio_masks],batch_first=True)
+        
+        
+        text_inputs = text_tokens.view(text_tokens.size(0)*text_tokens.size(1), text_tokens.size(2))
+        text_masks = text_masks.view(text_masks.size(0)*text_masks.size(1), text_masks.size(2))
+        audio_inputs = audio_inputs.view(audio_inputs.size(0)*audio_inputs.size(1), audio_inputs.size(2))
+        audio_masks = audio_masks.view(audio_masks.size(0)*audio_masks.size(1), audio_masks.size(2))
+
+        
+
+        return {
+            # text
+            "text_tokens": text_inputs,
+            "text_masks": text_masks,
+            # audio
+            "audio_inputs": audio_inputs,
+            "audio_masks": audio_masks,
+            # labels
+            "targets": 
+                torch.tensor(targets_M, dtype=torch.float32),
+        }
+
+
+class IEMOCAPDataset(torch.utils.data.Dataset):
+    def __init__(self, train=True):
+        self.videoIDs, self.videoSpeakers, self.videoLabels, self.videoText,\
+        self.roberta2, self.roberta3, self.roberta4, \
+        self.videoAudio, self.videoVisual, self.videoSentence, self.trainVid,\
+        self.testVid = pickle.load(open('data/iemocap_multimodal_features.pkl', 'rb'), encoding='latin1')
+        self.keys = [x for x in (self.trainVid if train else self.testVid)]
+
+        self.len = len(self.keys)
+
+    def __getitem__(self, index):
+        vid = self.keys[index]
+        return torch.FloatTensor(self.videoText[vid]),\
+               torch.FloatTensor(self.videoVisual[vid]),\
+               torch.FloatTensor(self.videoAudio[vid]),\
+               torch.FloatTensor([[1,0] if x=='M' else [0,1] for x in\
+                                  self.videoSpeakers[vid]]),\
+               torch.FloatTensor([1]*len(self.videoLabels[vid])),\
+               torch.LongTensor(self.videoLabels[vid]),\
+               vid
+
+    def __len__(self):
+        return self.len
+
+    def collate_fn(self, data):
+        dat = pd.DataFrame(data)
+        return [pad_sequence(dat[i]) if i<4 else pad_sequence(dat[i], True) if i<6 else dat[i].tolist() for i in dat]
+
+
+class MELDDataset(torch.utils.data.Dataset):
+    def __init__(self, path, train=True):
+        self.videoIDs, self.videoSpeakers, self.videoLabels, self.videoText, \
+        self.roberta2, self.roberta3, self.roberta4, \
+        self.videoAudio, self.videoVisual, self.videoSentence, self.trainVid,\
+        self.testVid, _ = pickle.load(open(path, 'rb'))
+
+        self.keys = [x for x in (self.trainVid if train else self.testVid)]
+
+        self.len = len(self.keys)
+        
+        self.feature_extractor = Wav2Vec2FeatureExtractor(feature_size=1, sampling_rate=16000, padding_value=0.0, do_normalize=True, return_attention_mask=True)
+        self.teokenizer = AutoTokenizer.from_pretrained("roberta-large")
+        
+    def __getitem__(self, index):
+        vid = self.keys[index]
+        return torch.FloatTensor(self.videoText[vid]),\
+               torch.FloatTensor(self.videoVisual[vid]),\
+               torch.FloatTensor(self.videoAudio[vid]),\
+               torch.FloatTensor(self.videoSpeakers[vid]),\
+               torch.FloatTensor([1]*len(self.videoLabels[vid])),\
+               torch.LongTensor(self.videoLabels[vid]),\
+               vid
+
+    def __len__(self):
+        return self.len
+
+    def return_labels(self):
+        return_label = []
+        for key in self.keys:
+            return_label+=self.videoLabels[key]
+        return return_label
+
+    def collate_fn(self, data):
+        dat = pd.DataFrame(data)
+        return [pad_sequence(dat[i]) if i<4 else pad_sequence(dat[i], True) if i<6 else dat[i].tolist() for i in dat]
+
+class Dataset_sims(torch.utils.data.Dataset):
+    # Argument List
+    #  csv_path: path to the csv file
+    #  audio_directory: path to the audio files
+    #  mode: train, test, valid
+    
+    def __init__(self, csv_path, audio_directory, mode):       
+        df = pd.read_csv(csv_path)
+        df = df[df['mode']==mode].reset_index()
+        
+        # store labels
+        self.targets_M = df['label']
+        
+        # store texts
+        self.texts = df['text']
+        self.tokenizer = AutoTokenizer.from_pretrained("hfl/chinese-roberta-wwm-ext")
+        
+        # store audio
+        self.audio_file_paths = []
+
+        for i in range(0,len(df)):
+            clip_id = str(df['clip_id'][i])
+            for j in range(4-len(clip_id)):
+                clip_id = '0'+clip_id
+            file_name = str(df['video_id'][i]) + '/' + clip_id + '.wav'
+            file_path = audio_directory + "/" + file_name
+            self.audio_file_paths.append(file_path)
+      
+        self.feature_extractor = Wav2Vec2FeatureExtractor(feature_size=1, sampling_rate=16000, padding_value=0.0, do_normalize=True, return_attention_mask=True)   
+        
+        
+    def __getitem__(self, index):
+       # extract text features
+        text = str(self.texts[index])         
+        tokenized_text = self.tokenizer(
+            text,            
+            max_length = 64,                                
+            padding = "max_length",     # Pad to the specified max_length. 
+            truncation = True,          # Truncate to the specified max_length. 
+            add_special_tokens = True,  # Whether to insert [CLS], [SEP], <s>, etc.   
+            return_attention_mask = True            
+        )               
+                
+        # extract audio features    
+        sound,_ = torchaudio.load(self.audio_file_paths[index])
+        soundData = torch.mean(sound, dim=0, keepdim=False)
+        features = self.feature_extractor(soundData, sampling_rate=16000, max_length=96000,return_attention_mask=True,truncation=True, padding="max_length")
+        audio_features = torch.tensor(np.array(features['input_values']), dtype=torch.float32).squeeze()
+        audio_masks = torch.tensor(np.array(features['attention_mask']), dtype=torch.long).squeeze()
+            
+        return { # text
+                "text_tokens": tokenized_text["input_ids"],
+                "text_masks": tokenized_text["attention_mask"],
+                 # audio
+                "audio_inputs": audio_features,
+                "audio_masks": audio_masks,
+                 # labels
+                "target": {
+                    "M": self.targets_M[index],
+                }
+                }
+    
+    def __len__(self):
+        return len(self.targets_M)
+    
+    def collate_fn_sims(batch):   
+        text_tokens = []  
+        text_masks = []
+        audio_inputs = []  
+        audio_masks = []
+        
+        targets_M = []
+        targets_T = []
+        targets_A = []
+    
+        # organize batch
+        for i in range(len(batch)):
+            # text
+            text_tokens.append(batch[i]['text_tokens'])
+            text_masks.append(batch[i]['text_masks'])
+            #audio
+            audio_inputs.append(batch[i]['audio_inputs'])
+            audio_masks.append(batch[i]['audio_masks'])
+
+        # labels
+            targets_M.append(batch[i]['target']['M'])
+            targets_T.append(batch[i]['target']['T'])
+            targets_A.append(batch[i]['target']['A'])        
+        
+        return {
+                # text
+                "text_tokens": torch.tensor(text_tokens, dtype=torch.long),
+                "text_masks": torch.tensor(text_masks, dtype=torch.long),           
+                # audio
+                "audio_inputs": torch.stack(audio_inputs),
+                "audio_masks": torch.stack(audio_masks),
+                # labels
+                "targets": {
+                        "M": torch.tensor(targets_M, dtype=torch.float32),
+                        "T": torch.tensor(targets_T, dtype=torch.float32),
+                        "A": torch.tensor(targets_A, dtype=torch.float32)
+                    }
+                }   
+
+
+
+class Dataset_mosi(torch.utils.data.Dataset):
+    # Argument List
+    #  csv_path: path to the csv file
+    #  audio_directory: path to the audio files
+    #  mode: train, test, valid
+    #  text_context_length
+    #  audio_context_length
+    
+    def __init__(self, csv_path, audio_directory, mode, text_context_length=2, audio_context_length=1):
+        df = pd.read_csv(csv_path)
+        invalid_files = ['3aIQUQgawaI/12.wav', '94ULum9MYX0/2.wav', 'mRnEJOLkhp8/24.wav', 'aE-X_QdDaqQ/3.wav', '94ULum9MYX0/11.wav', 'mRnEJOLkhp8/26.wav']
+        for f in invalid_files:
+            video_id = f.split('/')[0]
+            clip_id = f.split('/')[1].split('.')[0]
+            df = df[~((df['video_id']==video_id) & (df['clip_id']==int(clip_id)))]
+
+        df = df[df['mode']==mode].sort_values(by=['video_id','clip_id']).reset_index()
+        
+        # store labels
+        self.targets_M = df['annotation']
+        
+        # store texts
+        df['text'] = df['text'].str[0]+df['text'].str[1::].apply(lambda x: x.lower())
+        self.texts = df['text']
+        self.tokenizer = AutoTokenizer.from_pretrained("roberta-large")
+
+        # store audio
+        self.audio_file_paths = []
+        ## loop through the csv entries
+        for i in range(0,len(df)):
+            file_name = str(df['video_id'][i])+'/'+str(df['clip_id'][i])+'.wav'
+            file_path = audio_directory + "/" + file_name
+            self.audio_file_paths.append(file_path)
+        self.feature_extractor = Wav2Vec2FeatureExtractor(feature_size=1, sampling_rate=16000, padding_value=0.0, do_normalize=True, return_attention_mask=True)
+
+        # store context
+        self.video_id = df['video_id']
+        
+    def __getitem__(self, index):
+        # load text
+        text = str(self.texts[index])             
+
+        # tokenize text
+        tokenized_text = self.tokenizer(
+                text,            
+                max_length = 96,                                
+                padding = "max_length",     # Pad to the specified max_length. 
+                truncation = True,          # Truncate to the specified max_length. 
+                add_special_tokens = True,  # Whether to insert [CLS], [SEP], <s>, etc.   
+                return_attention_mask = True            
+            )  
+
+        # load audio
+        sound,_ = torchaudio.load(self.audio_file_paths[index])
+        soundData = torch.mean(sound, dim=0, keepdim=False)
+
+
+        # extract audio features
+        features = self.feature_extractor(soundData, sampling_rate=16000, max_length=96000,return_attention_mask=True,truncation=True, padding="max_length")
+        audio_features = torch.tensor(np.array(features['input_values']), dtype=torch.float32).squeeze()
+        audio_masks = torch.tensor(np.array(features['attention_mask']), dtype=torch.long).squeeze()
+
+
+        return { # text
+                "text_tokens": torch.tensor(tokenized_text["input_ids"], dtype=torch.long),
+                "text_masks": torch.tensor(tokenized_text["attention_mask"], dtype=torch.long),
+                # audio
+                "audio_inputs": audio_features,
+                "audio_masks": audio_masks,
+                 # labels
+                "targets": torch.tensor(self.targets_M[index], dtype=torch.float),
+                }
+    
+    def __len__(self):
+        return len(self.targets_M)
 
 
 def data_loader(batch_size):
     # csv_path = 'data/labell.csv'
     # audio_file_path = "data/OnlyCustomer"
-    # data = Dataset_audio_text(csv_path, audio_file_path)
+    # data = QA_Dataset(csv_path, audio_file_path)
     # train_data, test_data, val_data = torch.utils.data.random_split(
     #     data, [int(0.8*len(data)), int(0.1*len(data)), len(data)-int(0.8*len(data))-int(0.1*len(data))])
     train_label_path = '/home/lhy/MM-LLMs/MM-purchase-judgment/MMML/data/qa_data/train/dialog_test_11.csv'
@@ -189,9 +439,9 @@ def data_loader(batch_size):
     test_file_path = '/home/lhy/MM-LLMs/MM-purchase-judgment/MMML/data/qa_data/test/dialog'
     verify_file_path = '/home/lhy/MM-LLMs/MM-purchase-judgment/MMML/data/qa_data/verify/dialog'
     
-    train_data = Dataset_audio_text(train_label_path, train_file_path)
-    test_data = Dataset_audio_text(test_label_path, test_file_path)
-    val_data = Dataset_audio_text(verify_label_path, verify_file_path)
+    train_data = QA_Dataset(train_label_path, train_file_path)
+    test_data = QA_Dataset(test_label_path, test_file_path)
+    val_data = QA_Dataset(verify_label_path, verify_file_path)
     
     
     train_loader = DataLoader(
